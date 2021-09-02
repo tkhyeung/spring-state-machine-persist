b19e690ba5f74763a4ce744c6138844a82f99b5c
hkinnkun <hkinnkun@gmail.com>
2021-09-03 01:35:02 +0800

    add triggering same event to different states

--- src/main/java/com/example/sm/state_machine/FormStateMachineConfig.java
+++ src/main/java/com/example/sm/state_machine/FormStateMachineConfig.java
@@ -24,40 +24,37 @@ public class FormStateMachineConfig extends StateMachineConfigurerAdapter<String
 //                .states(EnumSet.allOf(FormStates.class));
                 .states(
                         EnumSet.allOf(FormStates.class).stream()
-                        .map(FormStates::name).collect(Collectors.toSet())
+                                .map(FormStates::name).collect(Collectors.toSet())
                 );
     }
 
     @Override
     public void configure(StateMachineTransitionConfigurer<String, String> transitions) throws Exception {
         transitions
-                .withExternal()
-                .source(FormStates.INIT.name())
-                .target(FormStates.SUBMITTED.name())
-                .event(FormEvents.SUBMIT.name())
-                .action(this::action1, this::error)
-                .guard(this::evaluate)
+                .withExternal().source(FormStates.INIT.name()).target(FormStates.SUBMITTED.name()).event(FormEvents.SUBMIT.name()).action(this::action1, this::error).guard(this::evaluate)
                 .and()
-                .withExternal()
-                .source(FormStates.SUBMITTED.name())
-                .target(FormStates.REVIEWED.name())
-                .event(FormEvents.REVIEW.name())
+                .withExternal().source(FormStates.SUBMITTED.name()).target(FormStates.REVIEWED.name()).event(FormEvents.REVIEW.name())
                 .and()
-                .withExternal()
-                .source(FormStates.REVIEWED.name())
-                .target(FormStates.APPROVED.name())
-                .event(FormEvents.APPROVE.name())
+                .withExternal().source(FormStates.REVIEWED.name()).target(FormStates.APPROVED.name()).event(FormEvents.APPROVE.name())
+
+                .and()
+                .withExternal().source(FormStates.INIT.name()).target(FormStates.APPROVED.name()).event(FormEvents.APPROVE.name()).action(this::action2, this::error)
         ;
 
+    }
+
+    private void error(StateContext<String, String> stringStringStateContext) {
+        log.info("error");
+    }
+
+    private void action2(StateContext<String, String> stringStringStateContext) {
+        log.info(stringStringStateContext + "");
+        log.info("action2");
     }
 
     private boolean evaluate(StateContext<String, String> stringStringStateContext) {
         log.info("evaluate");
         return false;
-    }
-
-    private void error(StateContext<String, String> stringStringStateContext) {
-        log.info("error");
     }
 
     private void action1(StateContext<String, String> stringStringStateContext) {
